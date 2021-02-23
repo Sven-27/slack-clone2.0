bdf8e3a18417b2576581ba855d0fd1733fa34914
Sven Notermans <sven.notermans@live.nl>
2021-02-23 22:39:22 +0100

    Made header and sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-
 import './App.css';
+import  { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Chat from './components/Chat'
+import Login from './components/Login'
+import styled from 'styled-components'
+import Header from './components/Header'
+import Sidebar from './components/Sidebar'
 
 function App() {
   return (
     <div className="app">
-      <h1>THIS IS THE APP COMPONENT</h1>
+      <Router>
+      <Container>
+        <Header />
+          <Main>
+            <Sidebar />
+          
+          <Switch>
+            <Route path="/room">
+                <Chat />
+            </Route>
+            <Route exact path="/">
+                <Login />
+            </Route>
+          </Switch>
+          </Main>
+        </Container>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+width: 100%;
+height: 100vh;
+display: grid;
+grid-template-rows: 38px auto;
+`
+
+const Main = styled.div`
+display: grid;
+grid-template-columns: 260px auto;
+`
+
---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Login.css'
+
+function Login() {
+    return (
+        <div className="login">
+            login
+        </div>
+    )
+}
+
+export default Login
